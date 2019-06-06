e7f9a7d685e12000d6ca4edea7a6601cff35650b
kylon <kylonux@gmail.com>
2019-06-06 19:19:07 +0200

    Update AutoGenerated.c

--- macserial/src/modelinfo_autogen.h
+++ macserial/src/modelinfo_autogen.h
@@ -6,115 +6,115 @@
 #include "macserial.h"
 
 typedef enum {
-  MacBook1_1, // Intel Core Duo T2400
-  MacBook10_1, // Intel Core M3 7Y32
-  MacBook2_1, // Intel Core 2 Duo T5600
-  MacBook3_1, // Intel Core 2 Duo T7500
-  MacBook4_1, // Intel Core 2 Duo T8300
-  MacBook5_1, // Intel Core 2 Duo P8600
-  MacBook5_2, // Intel Core 2 Duo P7450
-  MacBook6_1, // Intel Core 2 Duo P7550
-  MacBook7_1, // Intel Core 2 Duo P8600
-  MacBook8_1, // Intel Core M 5Y51
+  MacBook1_1, // Intel Core Duo T2400 @ 1.83 GHz
+  MacBook10_1, // Intel Core m3-7Y32 @ 1.10 GHz
+  MacBook2_1, // Intel Core 2 Duo T5600 @ 1.83 GHz
+  MacBook3_1, // Intel Core 2 Duo T7500 @ 2.20 GHz
+  MacBook4_1, // Intel Core 2 Duo T8300 @ 2.40 GHz
+  MacBook5_1, // Intel Core 2 Duo P8600 @ 2.40 GHz
+  MacBook5_2, // Intel Core 2 Duo P7450 @ 2.13 GHz
+  MacBook6_1, // Intel Core 2 Duo P7550 @ 2.26 GHz
+  MacBook7_1, // Intel Core 2 Duo P8600 @ 2.40 GHz
+  MacBook8_1, // Intel Core M 5Y51 @ 1.10 GHz
   MacBook9_1, // Intel Core m3-6Y30 @ 1.10 GHz
-  MacBookAir1_1, // Intel Core 2 Duo P7500
-  MacBookAir2_1, // Intel Core 2 Duo SL9600
-  MacBookAir3_1, // Intel Core 2 Duo SU9400
-  MacBookAir3_2, // Intel Core 2 Duo SL9400
-  MacBookAir4_1, // Intel Core i5 2467M
-  MacBookAir4_2, // Intel Core i5 2557M
-  MacBookAir5_1, // Intel Core i5 3317U
-  MacBookAir5_2, // Intel Core i5-3317U CPU @ 1.70GHz
-  MacBookAir6_1, // Intel Core i5 4250U
-  MacBookAir6_2, // Intel Core i5 4250U
-  MacBookAir7_1, // Intel Core i5 5250U
-  MacBookAir7_2, // Intel Core i5 5250U
+  MacBookAir1_1, // Intel Core 2 Duo P7500 @ 1.60 GHz
+  MacBookAir2_1, // Intel Core 2 Duo SL9600 @ 2.13 GHz
+  MacBookAir3_1, // Intel Core 2 Duo SU9400 @ 1.40 GHz
+  MacBookAir3_2, // Intel Core 2 Duo SL9400 @ 1.86 GHz
+  MacBookAir4_1, // Intel Core i5-2467M @ 1.60 GHz
+  MacBookAir4_2, // Intel Core i5-2557M @ 1.70 GHz
+  MacBookAir5_1, // Intel Core i5-3317U @ 1.70 GHz
+  MacBookAir5_2, // Intel Core i5-3317U @ 1.70GHz
+  MacBookAir6_1, // Intel Core i5-4250U @ 1.30 GHz
+  MacBookAir6_2, // Intel Core i5-4250U @ 1.30 GHz
+  MacBookAir7_1, // Intel Core i5-5250U @ 1.60 GHz
+  MacBookAir7_2, // Intel Core i5-5250U @ 1.60 GHz
   MacBookAir8_1, // Intel Core i5-8210Y @ 1.60 GHz
-  MacBookPro1_1, // Intel Core Duo L2400
-  MacBookPro1_2, // Intel Core Duo T2600
+  MacBookPro1_1, // Intel Core Duo L2400 @ 1.66 GHz
+  MacBookPro1_2, // Intel Core Duo T2600 @ 2.16 GHz
   MacBookPro10_1, // Intel Core i7-3615QM @ 2.30 GHz
   MacBookPro10_2, // Intel Core i5-3210M @ 2.50 GHz
-  MacBookPro11_1, // Intel Core i5 4258U
-  MacBookPro11_2, // Intel Core i7 4770HQ
-  MacBookPro11_3, // Intel Core i7 4850HQ
-  MacBookPro11_4, // Intel Core i7 4770HQ
-  MacBookPro11_5, // Intel Core i7 4870HQ
+  MacBookPro11_1, // Intel Core i5-4258U @ 2.40 GHz
+  MacBookPro11_2, // Intel Core i7-4770HQ @ 2.20 GHz
+  MacBookPro11_3, // Intel Core i7-4850HQ @ 2.30 GHz
+  MacBookPro11_4, // Intel Core i7-4770HQ @ 2.20 GHz
+  MacBookPro11_5, // Intel Core i7-4870HQ @ 2.50 GHz
   MacBookPro12_1, // Intel Core i5-5257U @ 2.70 GHz
   MacBookPro13_1, // Intel Core i5-6360U @ 2.00 GHz
-  MacBookPro13_2, // Intel Core i7 6567U
+  MacBookPro13_2, // Intel Core i7-6567U @ 3.30 GHz
   MacBookPro13_3, // Intel Core i7-6700HQ @ 2.60 GHz
   MacBookPro14_1, // Intel Core i5-7360U @ 2.30 GHz
-  MacBookPro14_2, // Intel Core i5 7267U
-  MacBookPro14_3, // Intel Core i7 7700HQ
+  MacBookPro14_2, // Intel Core i5-7267U @ 3.10 GHz
+  MacBookPro14_3, // Intel Core i7-7700HQ @ 2.80 GHz
   MacBookPro15_1, // Intel Core i7-8750H @ 2.20 GHz
   MacBookPro15_2, // Intel Core i7-8559U @ 2.70 GHz
-  MacBookPro2_1, // Intel Core 2 Duo T7600
-  MacBookPro2_2, // Intel Core 2 Duo T7400
-  MacBookPro3_1, // Intel Core 2 Duo T7700
-  MacBookPro4_1, // Intel Core 2 Duo T8300
-  MacBookPro5_1, // Intel Core 2 Duo P8600
-  MacBookPro5_2, // Intel Core 2 Duo T9600
-  MacBookPro5_3, // Intel Core 2 Duo P8800
-  MacBookPro5_4, // Intel Core 2 Duo P8700
-  MacBookPro5_5, // Intel Core 2 Duo P7550
-  MacBookPro6_1, // Intel Core i5 540M
-  MacBookPro6_2, // Intel Core i5 520M
-  MacBookPro7_1, // Intel Core 2 Duo P8600
-  MacBookPro8_1, // Intel Core i5 2415M
-  MacBookPro8_2, // Intel Core i7 2675QM
-  MacBookPro8_3, // Intel Core i7 2820QM
-  MacBookPro9_1, // Intel Core i7 3615QM
-  MacBookPro9_2, // Intel Core i5 3210M
-  MacPro1_1, // Intel Core Xeon 5130 @ 2.00 GHz x2
-  MacPro2_1, // Intel Xeon X5365 @ 3.00 GHz x2
-  MacPro3_1, // Intel Xeon E5462 @ 2.80 GHz x2
+  MacBookPro2_1, // Intel Core 2 Duo T7600 @ 2.33 GHz
+  MacBookPro2_2, // Intel Core 2 Duo T7400 @ 2.16 GHz
+  MacBookPro3_1, // Intel Core 2 Duo T7700 @ 2.40 GHz
+  MacBookPro4_1, // Intel Core 2 Duo T8300 @ 2.40 GHz
+  MacBookPro5_1, // Intel Core 2 Duo P8600 @ 2.40 GHz
+  MacBookPro5_2, // Intel Core 2 Duo T9600 @ 2.80 GHz
+  MacBookPro5_3, // Intel Core 2 Duo P8800 @ 2.66 GHz
+  MacBookPro5_4, // Intel Core 2 Duo P8700 @ 2.53 GHz
+  MacBookPro5_5, // Intel Core 2 Duo P7550 @ 2.26 GHz
+  MacBookPro6_1, // Intel Core i5-540M @ 2.53 GHz
+  MacBookPro6_2, // Intel Core i5-520M @ 2.40 GHz
+  MacBookPro7_1, // Intel Core 2 Duo P8600 @ 2.40 GHz
+  MacBookPro8_1, // Intel Core i5-2415M @ 2.30 GHz
+  MacBookPro8_2, // Intel Core i7-2675QM @ 2.20 GHz
+  MacBookPro8_3, // Intel Core i7-2820QM @ 2.30 GHz
+  MacBookPro9_1, // Intel Core i7-3615QM @ 2.30 GHz
+  MacBookPro9_2, // Intel Core i5-3210M @ 2.50 GHz
+  MacPro1_1, // Intel Core Xeon 5130 x2 @ 2.00 GHz
+  MacPro2_1, // Intel Xeon X5365 x2 @ 3.00 GHz
+  MacPro3_1, // Intel Xeon E5462 x2 @ 2.80 GHz
   MacPro4_1, // Intel Xeon W3520 @ 2.66 GHz
   MacPro5_1, // Intel Xeon W3530 @ 2.80 GHz
   MacPro6_1, // Intel Xeon E5-1620 v2 @ 3.70 GHz
-  Macmini1_1, // Intel Core Solo T1200
-  Macmini2_1, // Intel Core 2 Duo T5600
-  Macmini3_1, // Intel Core 2 Duo P7350
-  Macmini4_1, // Intel Core 2 Duo P8600
-  Macmini5_1, // Intel Core i5 2415M
-  Macmini5_2, // Intel Core i5 2520M
-  Macmini5_3, // Intel Core i7 2635QM
-  Macmini6_1, // Intel Core i5 3210M
-  Macmini6_2, // Intel Core i7 3615QM
-  Macmini7_1, // Intel Core i5 4260U
+  Macmini1_1, // Intel Core Solo T1200 @ 1.50 GHz
+  Macmini2_1, // Intel Core 2 Duo T5600 @ 1.83 GHz
+  Macmini3_1, // Intel Core 2 Duo P7350 @ 2.00 GHz
+  Macmini4_1, // Intel Core 2 Duo P8600 @ 2.40 GHz
+  Macmini5_1, // Intel Core i5-2415M @ 2.30 GHz
+  Macmini5_2, // Intel Core i5-2520M @ 2.50 GHz
+  Macmini5_3, // Intel Core i7-2635QM @ 2.00 GHz
+  Macmini6_1, // Intel Core i5-3210M @ 2.50 GHz
+  Macmini6_2, // Intel Core i7-3615QM @ 2.30 GHz
+  Macmini7_1, // Intel Core i5-4260U @ 1.40 GHz
   Macmini8_1, // Intel Core i7-8700B @ 3.20 GHz
-  Xserve1_1, // Intel Xeon 5130 x2
-  Xserve2_1, // Intel Xeon E5462 x2
-  Xserve3_1, // Intel Xeon E5520 x2
-  iMac10_1, // Intel Core 2 Duo E7600
-  iMac11_1, // Intel Core i5 750
-  iMac11_2, // Intel Core i3 540
-  iMac11_3, // Intel Core i5 760
-  iMac12_1, // Intel Core i5 2400S
-  iMac12_2, // Intel Core i7 2600
+  Xserve1_1, // Intel Xeon 5130 x2 @ 2.00 GHz
+  Xserve2_1, // Intel Xeon E5462 x2 @ 2.80 GHz
+  Xserve3_1, // Intel Xeon E5520 x2 @ 2.26 GHz
+  iMac10_1, // Intel Core 2 Duo E7600 @ 3.06 GHz
+  iMac11_1, // Intel Core i5-750 @ 2.70 GHz
+  iMac11_2, // Intel Core i3-540 @ 1.20 GHz
+  iMac11_3, // Intel Core i5-760 @ 2.80 GHz
+  iMac12_1, // Intel Core i5-2400S @ 2.50 GHz
+  iMac12_2, // Intel Core i7-2600 @ 2.80 GHz
   iMac13_1, // Intel Core i7-3770S @ 3.10 GHz
-  iMac13_2, // Intel Core i5 3470S
-  iMac13_3, // Intel Core i5 3470S
-  iMac14_1, // Intel Core i5 4570R
+  iMac13_2, // Intel Core i5-3470S @ 2.90 GHz
+  iMac13_3, // Intel Core i5-3470S @ 2.90 GHz
+  iMac14_1, // Intel Core i5-4570R @ 2.70 GHz
   iMac14_2, // Intel Core i7-4771 @ 3.50 GHz
-  iMac14_3, // Intel Core i5 4570S
-  iMac14_4, // Intel Core i5 4260U
+  iMac14_3, // Intel Core i5-4570S @ 2.90 GHz
+  iMac14_4, // Intel Core i5-4260U @ 1.40 GHz
   iMac15_1, // Intel Core i7-4790k @ 4.00 GHz
-  iMac16_1, // Intel Core i5 5250U
-  iMac16_2, // Intel Core i5 5675R
+  iMac16_1, // Intel Core i5-5250U @ 1.60 GHz
+  iMac16_2, // Intel Core i5-5675R @ 3.10 GHz
   iMac17_1, // Intel Core i5-6500 @ 3.20 GHz
   iMac18_1, // Intel Core i5-7360U @ 2.30 GHz
-  iMac18_2, // Intel Core i5 7400
+  iMac18_2, // Intel Core i5-7400 @ 2.40 GHz
   iMac18_3, // Intel Core i5-7600k @ 3.80 GHz
   iMac19_1, // Intel Core i7-9900K @ 3.60 GHz
-  iMac19_2, // Intel Core i5 8500
-  iMac4_1, // Intel Core Duo T2400
-  iMac4_2, // Intel Core Duo T2400
-  iMac5_1, // Intel Core 2 Duo T7200
-  iMac5_2, // Intel Core 2 Duo T5600
-  iMac6_1, // Intel Core 2 Duo T7400
-  iMac7_1, // Intel Core 2 Duo T7300
-  iMac8_1, // Intel Core 2 Duo E8435
-  iMac9_1, // Intel Core 2 Duo E8135
+  iMac19_2, // Intel Core i5-8500 @ 1.50 GHz
+  iMac4_1, // Intel Core Duo T2400 @ 1.83 GHz
+  iMac4_2, // Intel Core Duo T2400 @ 1.83 GHz
+  iMac5_1, // Intel Core 2 Duo T7200 @ 2.00 GHz
+  iMac5_2, // Intel Core 2 Duo T5600 @ 1.83 GHz
+  iMac6_1, // Intel Core 2 Duo T7400 @ 2.16 GHz
+  iMac7_1, // Intel Core 2 Duo T7300 @ 2.00 GHz
+  iMac8_1, // Intel Core 2 Duo E8435 @ 3.06 GHz
+  iMac9_1, // Intel Core 2 Duo E8135 @ 2.66 GHz
   iMacPro1_1, // Intel Xeon W-2140B CPU @ 3.20 GHz
 } AppleModel;
 
